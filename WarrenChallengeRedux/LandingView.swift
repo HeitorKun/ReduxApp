ab2a75306a0b7ca70a537c13c66626e944095ce5
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2022-11-14 10:27:46 -0300

    changed to fullScreenCover

--- WarrenChallengeRedux/LandingView.swift
+++ WarrenChallengeRedux/LandingView.swift
@@ -8,70 +8,64 @@
 import SwiftUI
 
 struct LandingView: View {
-    
-    @State var selection: Int? = nil
+    @State private var showingSheet = false
     
     var body: some View {
-        
-        NavigationStack {
-            VStack{
-                ZStack {
-                    Image("lakeHouse")
-                        .renderingMode(.original)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.8)
-                        .edgesIgnoringSafeArea(.all)
-                        .padding()
-                }.overlay(alignment: .bottom) {
-                    VStack{
-                        HStack{
-                            Text("Invista em você")
-                                .foregroundColor(.white)
-                                .font(.largeTitle)
-                                .bold()
-                            Spacer()
+            ZStack {
+                VStack{
+                    ZStack {
+                        Image("lakeHouse")
+                            .renderingMode(.original)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.8)
+                            .edgesIgnoringSafeArea(.all)
+                            .padding()
+                    }.overlay(alignment: .bottom) {
+                        VStack{
+                            HStack{
+                                Text("Invista em você")
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                                    .bold()
+                                Spacer()
+                            }
+                            HStack{
+                                Text("Invista toma tempo, mas não precisa ser o seu. Conte com os nossos especialistas")
+                                    .foregroundColor(.white)
+                                    .font(.body)
+                                Spacer()
+                            }
                         }
-                        HStack{
-                            Text("Invista toma tempo, mas não precisa ser o seu. Conte com os nossos especialistas")
-                                .foregroundColor(.white)
-                                .font(.body)
-                            Spacer()
-                        }
+                        .padding([.bottom], 50)
+                        .padding([.leading], 30)
+                        
                     }
-                    .padding([.bottom], 60)
-                    .padding([.leading], 30)
                     
+                    Spacer()
+                    
+                    
+                        Button {
+                            showingSheet.toggle()
+                        } label: {
+                            Text("Login")
+                                .padding([.bottom,.top])
+                                .font(.title3)
+                                .frame(minWidth: 0 , maxWidth: .infinity)
+                                .background(Color("redWarren"))
+                                .foregroundColor(.white)
+                                .cornerRadius(11)
+                        }
+                        .padding()
+                        .fullScreenCover(isPresented: $showingSheet, content: LoginView.init)
+                    
+                    Spacer()
                 }
-                .padding([.top], 0)
-                
-                NavigationLink(value: "LoginView") {
-                    Text("Login")
-                        .padding([.bottom,.top])
-                        .font(.title3)
-                        .frame(minWidth: 0 , maxWidth: .infinity)
-                        .background(Color("redWarren"))
-                        .foregroundColor(.white)
-                        .cornerRadius(11)
-                }
-                .navigationDestination(for: String.self) { view in
-                    if view == "LoginView" {
-                        LoginView()
-                            .navigationBarHidden(true)
-                    }
-                }
-                .padding()
-                .padding(.bottom, 50)
                 
             }
-            
-            
-        }.edgesIgnoringSafeArea(.top)
-        
+            .edgesIgnoringSafeArea(.top)
+        }
     }
-    
-}
-
 
 
 struct LandingView_Previews: PreviewProvider {
--- WarrenChallengeRedux/LoginView.swift
+++ WarrenChallengeRedux/LoginView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var isPresented = false
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
     }
 }
 
