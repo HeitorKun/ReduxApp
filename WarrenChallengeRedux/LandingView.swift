2d135d0f05d5e3976d7100759ab766044845d364
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2022-11-13 23:48:13 -0300

    landing page view done

---
+++ WarrenChallengeRedux/LandingView.swift
@@ -0,0 +1,72 @@
+//
+//  LandingView.swift
+//  WarrenChallengeRedux
+//
+//  Created by Heitor Feijó Kunrath on 13/11/22.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        ZStack {
+            VStack{
+                ZStack {
+                    Image("lakeHouse")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.8)
+                        .edgesIgnoringSafeArea(.all)
+                        .padding()
+                }.overlay(alignment: .bottom) {
+                    VStack{
+                        HStack{
+                            Text("Invista em você")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                                .bold()
+                            Spacer()
+                        }
+                        HStack{
+                            Text("Invista toma tempo, mas não precisa ser o seu. Conte com os nossos especialistas")
+                                .foregroundColor(.white)
+                                .font(.body)
+                            Spacer()
+                        }
+                    }
+                    .padding([.bottom], 50)
+                    .padding([.leading], 30)
+                    
+                }
+                
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .padding([.bottom,.top])
+                        .font(.title3)
+                        .frame(minWidth: 0 , maxWidth: .infinity)
+                        .background(Color("redWarren"))
+                        .foregroundColor(.white)
+                        .cornerRadius(11)
+                    
+                }
+                .padding()
+                
+                
+                Spacer()
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.top)
+        
+    }
+}
+
+struct LandingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+    }
+}
---
+++ WarrenChallengeRedux/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  WarrenChallengeRedux
+//
+//  Created by Heitor Feijó Kunrath on 13/11/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
