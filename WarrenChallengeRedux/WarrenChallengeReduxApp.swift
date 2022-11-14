e84fdd7ee16e31af2f8e6c2c4e0f7bbceb1ebffb
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2022-11-13 21:14:43 -0300

    init

---
+++ WarrenChallengeRedux/WarrenChallengeReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarrenChallengeReduxApp.swift
+//  WarrenChallengeRedux
+//
+//  Created by Heitor Feijó Kunrath on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WarrenChallengeReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
